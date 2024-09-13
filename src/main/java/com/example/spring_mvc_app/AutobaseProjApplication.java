b5c6e9b98a82ae2533084059961ac215e3457a54
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-09-13 12:59:02 +0300

    Just settings of spring MVC application

---
+++ src/main/java/com/example/spring_mvc_app/AutobaseProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobaseProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobaseProjApplication.class, args);
+	}
+
+}
